20e0296ee31a194185e4d49742ec592d25700a3a
Gyan Raj <bhardwaj7739@gmail.com>
2024-09-26 20:16:10 +0530

    mongoDB deployed

--- backend/index.js
+++ backend/index.js
@@ -7,21 +7,28 @@ const mongoose = require("mongoose");
 const taskRoutes = require("./routes/task.route");
 
 const app = express();
-const PORT = 8082;
-const DB_URI = "mongodb://localhost:27017/task-manager";
 
+// Use environment variables
+const PORT = process.env.PORT || 8082;
+const MONGODB_URL = process.env.MONGODB_URL;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Task routes
+app.use("/tasks", taskRoutes);
+
+// MongoDB connection and server startup
 mongoose
-  .connect(DB_URI, {
+  .connect(MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("DB Connected!"))
-  .catch((error) => console.log("Error in connecting DB", error));
-
-app.use(cors());
-app.use(express.json());
-app.use("/tasks", taskRoutes);
-
-app.listen(PORT, () => {
-  console.log(`Backend listening on Port ${PORT}!`);
-});
+  .then(() => {
+    console.log("Connected to database:", MONGODB_URL);
+    app.listen(PORT, () => {
+      console.log(`Server listening on PORT: ${PORT}`);
+    });
+  })
+  .catch((err) => console.log("Database connection error:", err));
